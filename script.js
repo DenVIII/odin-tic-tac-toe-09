b8bc19d1c5fb6144315f6b648407beae78268487
DenVIII <Denis226@gmail.com>
2024-01-28 19:33:51 +0300

    Add for loop which creates _board array

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const gameBoard = (function () {
-    const _board = [
-        ['-','-','-'],
-        ['-','-','-'],
-        ['-','-','-']
-    ]
+    const rows = 3;
+    const cols = 3;
+    const _board = [];
+
+    for (let i = 0; i < rows; i++) {
+        _board.push([]);
+        for (let j = 0; j < cols; j++) {
+            _board[i].push('-');
+        }
+    }
+
     function getBoard() {
         console.log(_board);
     }
